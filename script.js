858b540c5411a79e90aa1120a54f1737b422b11b
luis <lmejiaidep@gmail.com>
2026-04-24 14:24:37 -0500

    cambios

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const about=document.querySelector(".nav_about");
+const about = document.querySelector(".nav_about");
 //const resume=document.querySelector(".resume");
-const pets=document.querySelector(".nav_pets");
-const titulo= document.querySelector("#title_nav");
-
-const seccion_about=document.querySelector(".about_me");
-const seccion_pets=document.querySelector(".pets");
+const pets = document.querySelector(".nav_pets");
+const titulo = document.querySelector("#title_nav");
+const proyectos = document.querySelector(".proyects")
+const seccion_about = document.querySelector(".about_me");
+const seccion_pets = document.querySelector(".pets");
 
 about.addEventListener("click", () => fAct_Des("about"));
 //resume.addEventListener("click", () => fAct_Des("resume"));
@@ -12,27 +12,29 @@ pets.addEventListener("click", () => fAct_Des("pets"));
 
 
 
-function fAct_Des(info){
-    
-    if (info=="about"){
+function fAct_Des(info) {
+
+    if (info == "about") {
         seccion_about.classList.add("active");
         seccion_pets.classList.remove("active");
         about.classList.add("active");
         //resume.classList.remove("active")
         pets.classList.remove("active");
-        titulo.textContent="About Me";
-    }else if( info =="resume"){
+        proyectos.classList.add("active");
+        titulo.textContent = "About Me";
+    } else if (info == "resume") {
         about.classList.remove("active");
         //resume.classList.add("active")
         pets.classList.remove("active");
-        titulo.textContent="Resume";
-    }else{
+        titulo.textContent = "Resume";
+    } else {
         about.classList.remove("active");
         //resume.classList.remove("active")
         pets.classList.add("active");
-        titulo.textContent="My Pets";
+        titulo.textContent = "My Pets";
         seccion_pets.classList.add("active");
         seccion_about.classList.remove("active");
+        proyectos.classList.remove("active");
     }
 
 }
